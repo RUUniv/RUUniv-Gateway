8a266b713def2c3af2163816ed77eedfb24bf0cd
bandalgomsu <q63530@naver.com>
2024-08-13 17:39:22 +0900

    init

---
+++ src/main/java/com/ruunivgatewayserver/RuunivGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.ruunivgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuunivGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuunivGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruunivgatewayserver/RuunivGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruunivgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuunivGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
