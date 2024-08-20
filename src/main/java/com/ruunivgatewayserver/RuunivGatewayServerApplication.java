fbfe05bc8e519e0125a45a26a01efbccafd3d5df
bandalgomsu <q63530@naver.com>
2024-08-20 19:04:44 +0900

    infra : Eureka Client 재설정

--- src/main/java/com/ruunivgatewayserver/RuunivGatewayServerApplication.java
+++ src/main/java/com/ruunivgatewayserver/RuunivGatewayServerApplication.java
@@ -1,13 +1,17 @@
 package com.ruunivgatewayserver;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class RuunivGatewayServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RuunivGatewayServerApplication.class, args);
     }
 
 }
+
+
